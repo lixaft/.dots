6f4c05fa3dde0953baf7ceb6c2d6e5147e6950be
Fabien Taxil <me@lixaft.dev>
2025-06-09 15:38:36 +0200

    t

--- home/default.nix
+++ home/default.nix
@@ -18,6 +18,7 @@
 
     sessionVariables = {
       PAGER = "${lib.getExe pkgs.less} --chop-long-lines";
+      # Using full exe path does not seem to load the plugins...
       EDITOR = "nvim";
       LS_COLORS = flakeLib.concatAttrs "=" ":" {
         di = "00;36";
@@ -28,12 +29,6 @@
         "*.lock" = "00;90";
         "*~" = "00;90";
       };
-    };
-
-    shellAliases = {
-      # Fix a weird clipboard issue in wsl.
-      # TODO: I don't think this is needed anymore, just keeping around ATM.
-      fixway = "ln -s /mnt/wslg/runtime-dir/wayland-0* /run/user/1000/";
     };
 
     packages = import ./packages.nix pkgs;
