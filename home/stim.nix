1430e226681dc95827da65e890e522b3a6f2cf67
Fabien Taxil <me@lixaft.dev>
2025-09-18 19:33:28 +0200

    home(stim): setup vpn and rdp connection

--- home/stim.nix
+++ home/stim.nix
@@ -1,5 +1,15 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  home.shellAliases = {
+    stim-vpn = "sudo ${lib.getExe pkgs.openfortivpn} -c ${config.xdg.configHome}/openfortivpn/stim.conf";
+  };
+
   home.packages = with pkgs; [
     openfortivpn
+    remmina
   ];
 }
